203bd81d6615b267483d43a0433c133694063b32
SB <ijhollaw@uncg.edu>
2025-10-08 09:46:33 -0400

    spring initializr
    added the spring initializr files

---
+++ src/main/java/com/Assignment_Three/CRUD_API/CrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.Assignment_Three.CRUD_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Assignment_Three/CRUD_API/CrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Assignment_Three.CRUD_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
