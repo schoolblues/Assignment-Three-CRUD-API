10eb3a40bdd04d1fde98a43e3fb0bb5b429e39a6
SB <ijhollaw@uncg.edu>
2025-10-10 09:56:02 -0400

    updated animal repository
    added needed lists in the repo

--- src/main/java/com/Assignment_Three/CRUD_API/animal/AnimalRepository.java
+++ src/main/java/com/Assignment_Three/CRUD_API/animal/AnimalRepository.java
@@ -1,5 +1,18 @@
 package com.Assignment_Three.CRUD_API.animal;
 
-public class AnimalRepository {
-    
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnimalRepository extends JpaRepository<Animal, Long> {
+    List<Animal> getAnimalsByName(String name);
+
+    List<Animal> getAnimalsByAge(double age);
+
+    List<Animal> getAnimalsByWeight(double age);
+
+    List<Animal> getAnimalsByDescription(String description);
+
+    List<Animal> getAnimalsByGender(String gender);
 }
--- src/main/java/com/Assignment_Three/CRUD_API/animal/AnimalService.java
+++ src/main/java/com/Assignment_Three/CRUD_API/animal/AnimalService.java
@@ -1,13 +1,8 @@
 package com.Assignment_Three.CRUD_API.animal;
 
-import java.io.IOException;
-import java.io.File;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class AnimalService {
