63ff506060443366e852b3251be149c723068a73
SB <ijhollaw@uncg.edu>
2025-11-04 08:58:03 -0500

    Update AnimalController.java
    changed controller type

--- src/main/java/com/Assignment_Three/CRUD_API/animal/AnimalController.java
+++ src/main/java/com/Assignment_Three/CRUD_API/animal/AnimalController.java
@@ -3,6 +3,7 @@ package com.Assignment_Three.CRUD_API.animal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,9 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class AnimalController {
 
     @Autowired
